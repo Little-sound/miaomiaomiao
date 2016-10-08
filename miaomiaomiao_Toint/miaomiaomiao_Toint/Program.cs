4e79073dea2e19c5cdc693d4dc78de04c38208f6
Little-sound <littlesoundqvq@gmail.com>
2016-10-08 18:28:29 +0800

    上传了一个显示每个字编码的程序~
    你只要输入一个字符串，它就会告诉你每个字符的编码啦~

---
+++ miaomiaomiao_Toint/miaomiaomiao_Toint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miaomiaomiao_Toint {
+    class Program {
+        static void Main(string[] args) {
+            while (true) {
+                string shuRu = Console.ReadLine();
+                for (int i = 0; i < shuRu.Length; i++) {
+                    int j = shuRu[i];
+                    Console.Write("{0},", j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
